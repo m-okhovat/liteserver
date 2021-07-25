e2f7c5d317168f2a91f5c64be7e49b0a729fd91c
Mehdi Okhovat <Mehdi@DESKTOP-Q9JNDQL>
2021-07-25 11:09:33 +0430

    HttpServer start mothod  implemented

--- src/LiteServer/HttpListeners/HttpServer.cs
+++ src/LiteServer/HttpListeners/HttpServer.cs
@@ -1,7 +1,11 @@
 ﻿using LiteServer.Extensions;
 using LiteServer.Handlers;
+using LiteServer.Http.Extensions;
+using LiteServer.Http.Features;
+using LiteServer.Http.HttpContext;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Threading;
@@ -11,7 +15,7 @@ namespace LiteServer.HttpListeners
 {
     public class HttpServer : HttpServerBase
     {
-        private HttpListener _httpListener;
+        private readonly HttpListener _httpListener = new HttpListener();
         public HttpServer(IEnumerable<string> paths)
         {
             if (paths == null || !paths.Any()) throw new ArgumentNullException(nameof(paths));
@@ -29,7 +33,83 @@ namespace LiteServer.HttpListeners
 
         public override Task StartServer(HandlerDelegate handler, CancellationToken cancel)
         {
-            throw new NotImplementedException();
+            _httpListener.Start();
+            foreach (var httpListenerPrefix in _httpListener.Prefixes)
+            {
+                Console.WriteLine($"{nameof(HttpServer)} Server started listening on {httpListenerPrefix}");
+            }
+
+            return Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    Thread.CurrentThread.Name = "HttpListener Server";
+
+                    while (!cancel.IsCancellationRequested)
+                    {
+                        var getContextAsync = _httpListener.GetContextAsync();
+                        getContextAsync.Wait(cancel);
+
+                        var httpListenerContext = getContextAsync.Result;
+                        var httpListenerFeature = new HttpListenerFeature(httpListenerContext);
+                        var featureCollection = new FeatureCollection()
+                            .Set<IHttpRequestFeature>(httpListenerFeature)
+                            .Set<IHttpResponseFeature>(httpListenerFeature);
+                        var httpContext = new LiteHttpContext(featureCollection);
+
+                        Task.Factory.StartNew(async () =>
+                        {
+                            var response = httpListenerContext.Response;
+                            try
+                            {
+                                var httpListenerRequest = httpListenerContext.Request;
+                                Console.WriteLine(
+                                    $"[{DateTime.Now}] {httpListenerRequest.HttpMethod} Http request received at {httpListenerRequest.LocalEndPoint} {httpListenerRequest.Url}");
+
+                                await handler(httpContext);
+                            }
+                            catch (Exception e) when (!(e is OperationCanceledException))
+                            {
+                                if (!_httpListener.IsListening)
+                                    return;
+                                Trace.WriteLine($"Error in {nameof(HttpServer)}: {e}");
+
+                                try
+                                {
+                                    response.StatusCode = 500;
+                                }
+                                catch
+                                {
+                                    // Might be too late in request processing to set response code, so just ignore.
+                                }
+                            }
+                            catch (EntryPointNotFoundException e)
+                            {
+                                response.StatusCode = (int)HttpStatusCode.NotFound;
+                            }
+                            finally
+                            {
+                                response.Close();
+                            }
+                        }, cancel);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+                finally
+                {
+                    _httpListener.Stop();
+                    _httpListener.Close();
+                    foreach (var httpListenerPrefix in _httpListener.Prefixes)
+                    {
+                        Console.WriteLine(
+                            $"{nameof(HttpServer)} Server stopped listening on {httpListenerPrefix}");
+                    }
+                }
+            }, TaskCreationOptions.LongRunning);
         }
     }
 }
