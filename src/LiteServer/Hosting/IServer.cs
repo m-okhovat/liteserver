25aa115ea7c298bdd4a79bb6ed2094819cb0afec
Mehdi Okhovat <Mehdi@DESKTOP-Q9JNDQL>
2021-07-25 10:24:58 +0430

    IServer and HttpServerBase added

---
+++ src/LiteServer/Hosting/IServer.cs
@@ -0,0 +1,13 @@
+﻿using LiteServer.Handlers;
+using System;
+using System.Threading.Tasks;
+
+namespace LiteServer.Hosting
+{
+    public interface IServer : IDisposable
+    {
+        Task StartAsync(HandlerDelegate handler);
+        Task StopAsync();
+        void Stop();
+    }
+}
---
+++ src/LiteServer/HttpListeners/HttpServerBase.cs
@@ -0,0 +1,58 @@
+﻿using LiteServer.Handlers;
+using LiteServer.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiteServer.HttpListeners
+{
+    public abstract class HttpServerBase : IServer
+    {
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+        private Task _serverTask;
+
+        public Task StartAsync(HandlerDelegate handler)
+        {
+            if (_serverTask != null)
+                throw new InvalidOperationException("Server has been started already.");
+
+            _serverTask = StartServer(handler, _cts.Token);
+            return _serverTask;
+        }
+
+        public async Task StopAsync()
+        {
+            _cts?.Cancel();
+
+            try
+            {
+                if (_serverTask == null)
+                    return; // Never started.
+
+                // This will re-throw any exception that was caught on the StartServerAsync thread.
+                await _serverTask;
+            }
+            catch (OperationCanceledException)
+            {
+                //since it can easily get thrown by whatever checks the CancellationToken.
+            }
+            finally
+            {
+                _cts?.Dispose();
+                _cts = null;
+            }
+        }
+
+        public void Stop()
+        {
+            StopAsync().GetAwaiter().GetResult();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public abstract Task StartServer(HandlerDelegate handler, CancellationToken cancel);
+    }
+}
