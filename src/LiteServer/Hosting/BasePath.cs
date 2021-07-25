58cde25f360afd72fbb4f9458782549c8479874f
Mehdi Okhovat <Mehdi@DESKTOP-Q9JNDQL>
2021-07-25 13:08:16 +0430

    BasePath and basePathBuilder added

---
+++ src/LiteServer/Hosting/BasePath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LiteServer.Hosting
+{
+    public class BasePath
+    {
+        public string HostName { get; }
+        public int Port { get; }
+        public string Url { get; }
+        public bool UseHttps { get; }
+
+        public BasePath(string hostName, int port, string url, bool useHttps = false)
+        {
+            HostName = hostName ?? throw new ArgumentNullException(nameof(hostName));
+            Port = port;
+            UseHttps = useHttps;
+            Url = url;
+        }
+
+        public override string ToString()
+        {
+            var s = UseHttps ? "s" : string.Empty;
+            return $"http{s}://{HostName}:{Port}/{Url}";
+        }
+    }
+}
---
+++ src/LiteServer/Hosting/BasePathBuilder.cs
@@ -0,0 +1,40 @@
+﻿namespace LiteServer.Hosting
+{
+    public class BasePathBuilder
+    {
+        private string _hostName = "localhost";
+        private int _port = 5000;
+        private string _url = "";
+        private bool _useHttps = false;
+
+        public BasePathBuilder WithHostName(string hostName)
+        {
+            _hostName = hostName;
+            return this;
+        }
+
+        public BasePathBuilder WithPort(int port)
+        {
+            _port = port;
+            return this;
+        }
+
+        public BasePathBuilder WithUrl(string url)
+        {
+            _url = url;
+            return this;
+        }
+
+        public BasePathBuilder UseHttps()
+        {
+            _useHttps = true;
+            return this;
+        }
+
+        internal BasePath Build()
+        {
+            return new BasePath(_hostName, _port, _url, _useHttps);
+        }
+
+    }
+}
