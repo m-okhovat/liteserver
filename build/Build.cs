26c22addd0e4ae7c74a3af7a18261e16411d597d
Mehdi Okhovat <Mehdi@DESKTOP-Q9JNDQL>
2021-07-31 02:08:33 +0430

    Build automation pipeline added

--- build/Build.cs
+++ build/Build.cs
@@ -1,70 +1,158 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
 using Nuke.Common.Execution;
-using Nuke.Common.Git;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using Nuke.Common.Tools.NuGet;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+
 [CheckBuildProjectConfigurations]
-[ShutdownDotNetAfterServerBuild]
+[UnsetVisualStudioEnvironmentVariables]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.RunUnitTests);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "netcoreapp3.1")] GitVersion GitVersion;
+    [Parameter] string TestResultDirectory = RootDirectory + "/Artifacts/Test-Results/";
+    [Parameter] string NugetOutputDirectory = RootDirectory + "/Artifacts/NugetPackages/";
+    [Parameter] string NugetServerUrl = "https://api.nuget.org/v3/index.json";
+    [Parameter] string NugetApiKey;
 
-    AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath TestsDirectory => RootDirectory / "tests";
-    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
-
-    Target Clean => _ => _
-        .Before(Restore)
+    Target Information => _ => _
+        .Before(Preparation)
         .Executes(() =>
         {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(ArtifactsDirectory);
+            Logger.Info($"Configuration : {Configuration}");
+            Logger.Info($"TestResultDirectory : {TestResultDirectory}");
+            Logger.Info($"NugetOutputDirectory : {NugetOutputDirectory}");
+            Logger.Info($"NugetOutputDirectory : {NugetServerUrl}");
+            Logger.Info($"GitVersion.NuGetVersionV2 : {GitVersion.NuGetVersionV2}");
+        });
+
+    Target Preparation => _ => _
+        .DependsOn(Information)
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(TestResultDirectory);
+            EnsureCleanDirectory(NugetOutputDirectory);
+        });
+
+    Target Clean => _ => _
+        .DependsOn(Preparation)
+        .Executes(() =>
+        {
+            DotNetClean(a =>
+                a.SetProject(Solution)
+                    .SetConfiguration(Configuration));
+            EnsureCleanDirectory(NugetOutputDirectory);
         });
 
     Target Restore => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
         {
-            DotNetRestore(s => s
-                .SetProjectFile(Solution));
+            DotNetRestore(a => a.SetProjectFile(Solution));
         });
 
-    Target Compile => _ => _
+    Target UpdateVersion => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .SetAssemblyVersion(GitVersion.AssemblySemVer)
-                .SetFileVersion(GitVersion.AssemblySemFileVer)
-                .SetInformationalVersion(GitVersion.InformationalVersion)
-                .EnableNoRestore());
+            const string pattern = @"<Version>(.*)<\/Version>";
+            const string fileName = "Directory.Build.Props";
+            var filePath = Path.Combine(Solution.Directory, fileName);
+            var content = GetFileContent(filePath);
+            var version = "<Version>" + GitVersion.AssemblySemVer + "</Version>";
+            content = Regex.Replace(content, pattern, version);
+            File.WriteAllText(filePath, content, Encoding.UTF8);
         });
 
+
+    Target Compile => _ => _
+        .DependsOn(UpdateVersion)
+        .Executes(() =>
+        {
+            DotNetBuild(a =>
+                a.SetProjectFile(Solution)
+                    .SetNoRestore(true)
+                    .SetConfiguration(Configuration));
+        });
+
+    Target RunUnitTests => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            var testProjects = Solution.AllProjects.Where(s => s.Name.Contains("Tests.Unit"));
+
+            DotNetTest(a => a
+                .SetConfiguration(Configuration)
+                .SetNoBuild(true)
+                .SetNoRestore(true)
+                .ResetVerbosity()
+                .SetResultsDirectory(TestResultDirectory)
+                    .EnableCollectCoverage()
+                    .SetCoverletOutputFormat(CoverletOutputFormat.opencover)
+                    .SetExcludeByFile("*.Generated.cs")
+                    .EnableUseSourceLink()
+                .CombineWith(testProjects, (b, z) => b
+                    .SetProjectFile(z)
+                    .SetLogger($"trx;LogFileName={z.Name}.trx")
+                    .SetCoverletOutput(TestResultDirectory + $"{z.Name}.xml")));
+        });
+
+    Target PackNugetPackages => _ => _
+        .DependsOn(RunUnitTests)
+        .Executes(() =>
+        {
+            var projectsToPack = Solution.AllProjects
+                .Where(s => !s.Name.Contains("Tests", StringComparison.OrdinalIgnoreCase)
+                            && !s.Name.Contains("Sample", StringComparison.OrdinalIgnoreCase)
+                            && !s.Name.Contains("build", StringComparison.OrdinalIgnoreCase));
+
+            foreach (var project in projectsToPack)
+            {
+                DotNetPack(s => s
+                    .SetProject(project)
+                    .SetOutputDirectory(NugetOutputDirectory)
+                    .SetNoBuild(true)
+                    .SetNoRestore(true)
+                    .SetConfiguration(Configuration)
+                    .SetVersion(GitVersion.NuGetVersionV2));
+            }
+        });
+
+    Target PushNugetPackages => _ => _
+        .DependsOn(PackNugetPackages)
+        .Executes(() =>
+        {
+            var nugetFiles = GlobFiles(NugetOutputDirectory, "*.nupkg");
+
+            foreach (var file in nugetFiles)
+            {
+                DotNetNuGetPush(a => a
+                        .SetApiKey(NugetApiKey)
+                        .SetSource(NugetServerUrl)
+                        .SetTargetPath(file));
+            }
+        });
+
+    string GetFileContent(string filePath)
+    {
+        var content = File.ReadAllText(filePath, Encoding.UTF8);
+        return content;
+    }
 }
